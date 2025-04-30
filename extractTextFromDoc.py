f53a87ee2d6fc2b9881e3b4e00bd1bda96846874
svarkas <stvarkas@gmail.com>
2025-04-30 15:04:09 +0300

    replacing special characters in composite file's output to ensure parsing

--- extractTextFromDoc.py
+++ extractTextFromDoc.py
@@ -3,6 +3,7 @@ import sys
 import libs.OldDoc as OD
 import libs.DocXtoTXT as DXT
 import magic
+import re
 
 def main(argv):
     inputFile=argv[1]
@@ -10,7 +11,11 @@ def main(argv):
     file_type_info = magic.from_file(inputFile)
     if "Composite Document File V2 Document" in file_type_info: 
         od = OD.OldDoc()
-        print(od.extractText(inputFile))
+        man_text = od.extractText(inputFile)
+        man_text = man_text.replace("\xa0", " ").replace("\u200b", "")
+        man_text = man_text.replace("\r\n", "\n").replace("\r", "\n")
+        man_text = re.sub(r"[^\x09\x0A\x0D\x20-\x7E]", "", man_text)
+        print(man_text)
     elif file_type_info == "Microsoft Word 2007+":
         dxt = DXT.DocXtoTXT()
         print(dxt.get_docx_txt(inputFile))
